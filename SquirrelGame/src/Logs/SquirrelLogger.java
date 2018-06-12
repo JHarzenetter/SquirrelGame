8366c96dd1360cc89772afefe9b7da9ce5219adc
JHarzenetter <johannes.harzenetter@gmail.com>
2018-06-12 11:44:38 +0200

    JavaDoc

--- SquirrelGame/src/Logs/SquirrelLogger.java
+++ SquirrelGame/src/Logs/SquirrelLogger.java
@@ -3,7 +3,6 @@ package Logs;
 import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 public class SquirrelLogger {
 
@@ -14,7 +13,6 @@ public class SquirrelLogger {
         try {
             fh = new FileHandler("C:\\SquirrelLogs\\LogFile.txt");
             log.addHandler(fh);
-            fh.setFormatter(new SimpleFormatter());
         } catch (IOException e) {
             e.printStackTrace();
         }
